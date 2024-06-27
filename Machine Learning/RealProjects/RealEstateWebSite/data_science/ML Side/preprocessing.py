7a3139277c93bba02c1db7f31f6f1c7e956b9576
Venchislaw <venchislavcodes@gmail.com>
2024-06-27 15:30:58 +0300

    NOWAY

--- Machine Learning/RealProjects/RealEstateWebSite/data_science/ML Side/preprocessing.py
+++ Machine Learning/RealProjects/RealEstateWebSite/data_science/ML Side/preprocessing.py
@@ -1,15 +1,22 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import RobustScaler, OrdinalEncoder
+from sklearn.preprocessing import RobustScaler, OrdinalEncoder, OneHotEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
+
 
 num_feats = []
 cat_feats = []
 ohe_cat_feats = []
 
 
+# a
+pd.set_option("display.max_columns", 500)
+
+
+# this function fills feature arrays
+# these arrays will be used in preprocessing
 def feature_cluster(df):
     for feature in df.columns:
         if df[feature].isnull().sum() >= df.shape[0] * 0.5:
@@ -21,84 +28,83 @@ def feature_cluster(df):
                 ohe_cat_feats.append(feature)
         else:
             num_feats.append(feature)
-    # num_feats.remove("SalePrice")
+    # Id feature is just useless
     num_feats.remove("Id")
-    print(num_feats)
 
 
-def preprocess_data(data_path):
+def preprocess(data_path):
     df = pd.read_csv(data_path)
-    # classify features
     feature_cluster(df)
-
-    df = df[num_feats + cat_feats]
+    # print("NoSeWa" in cat_feats)
 
     X = df.drop("SalePrice", axis=1)
     y = df["SalePrice"]
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
 
-    X_train = pd.DataFrame(X_train, columns=df.columns)
-    print(X_train.head())
-    X_test = pd.DataFrame(X_test, columns=df.columns)
+    # selecting numeric features
 
-    numeric_df = df[num_feats]
-    categoric_df = df[cat_feats]
+    num_feats.remove("SalePrice")
+    X_train_num = X_train[num_feats]
+    X_test_num = X_test[num_feats]
 
-    num_pipeline = make_pipeline(
+    transform_pipeline = make_pipeline(
         SimpleImputer(strategy="mean"),
         RobustScaler()
     )
 
-    cat_pipeline = make_pipeline(
-        SimpleImputer(strategy="most_frequent"),
-        OrdinalEncoder()
-    )
+    X_train_num = transform_pipeline.fit_transform(X_train_num)
+    X_test_num = transform_pipeline.transform(X_test_num)
 
-    print(X_train.shape, len(cat_feats), len(num_feats))
-    X_train_num = X_train[num_feats]
-    X_test_num = X_test[num_feats]
+    X_train_num = pd.DataFrame(X_train_num, columns=num_feats)
+    X_test_num = pd.DataFrame(X_test_num, columns=num_feats)
+
+
+    # categorical data preprocessing
+
+    imputer = SimpleImputer(strategy="most_frequent")
+    encoder = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
 
     X_train_cat = X_train[cat_feats]
     X_test_cat = X_test[cat_feats]
 
-    num_feats.remove("SalePrice")
-    # numero
-    X_train_num = pd.DataFrame(
-        num_pipeline.fit_transform(X_train_num),
-        columns=num_feats
-    )
+    # impute values
+    X_train_cat = imputer.fit_transform(X_train_cat)
+    X_test_cat = imputer.transform(X_test_cat)
 
-    X_test_num = pd.DataFrame(
-        num_pipeline.transform(X_test_num),
-        columns=num_feats
-    )
+    # encode values
+    X_train_cat = encoder.fit_transform(X_train_cat)
+    X_test_cat = encoder.transform(X_test_cat)
 
-    # categorical
-    # cat_pipeline.fit_transform(X_train_cat)
-    X_train_cat = pd.DataFrame(
-        cat_pipeline.fit_transform(X_train_cat),
-        columns=num_feats
-    )
+    X_train_cat_ohe = X_train[ohe_cat_feats]
+    X_test_cat_ohe = X_test[ohe_cat_feats]
 
-    X_test_cat = pd.DataFrame(
-        cat_pipeline.transform(X_test_cat),
-        columns=num_feats
-    )
+    X_train_cat_ohe = pd.get_dummies(X_train_cat_ohe, dtype="int")
+    X_test_cat_ohe = pd.get_dummies(X_test_cat_ohe, dtype="int")
 
-    X_train_ohe_cat = pd.get_dummies(X_train_cat[ohe_cat_feats])
-    X_test_ohe_cat = pd.get_dummies(X_test_cat[ohe_cat_feats])
 
-    X_train_cat = pd.merge(X_train_cat, X_train_ohe_cat, left_index=True, right_index=True)
-    X_test_cat = pd.merge(X_test_cat, X_test_ohe_cat, left_index=True, right_index=True)
+    X_train_cat = pd.DataFrame(X_train_cat, columns=cat_feats)
+    X_test_cat = pd.DataFrame(X_test_cat, columns=cat_feats)
 
-    X_train_cat = X_train.drop(ohe_cat_feats, axis=1)
-    X_test_cat = X_test.drop(ohe_cat_feats, axis=1)
+    X_train_cat = pd.merge(X_train_cat, X_train_cat_ohe, left_index=True, right_index=True)
+    X_test_cat = pd.merge(X_test_cat, X_test_cat_ohe, left_index=True, right_index=True)
 
+
+    # gotta fix that
+    """print(set(X_train_cat.columns).difference(set(ohe_cat_feats)))
+    print(ohe_cat_feats)
+    print("-" * 60)
+    print(X_train_cat[ohe_cat_feats].head())
+
+    X_train_cat = X_train_cat.drop(ohe_cat_feats, axis=1)
+    X_test_cat = X_test_cat.drop(ohe_cat_feats, axis=1)"""
+
+    # merge two dataframes
     X_train = pd.merge(X_train_num, X_train_cat, left_index=True, right_index=True)
     X_test = pd.merge(X_test_num, X_test_cat, left_index=True, right_index=True)
 
-    return X_train, X_test
+    return X_train, X_test, y_train, y_test
 
 
-preprocess_data("/media/venchislav/Говорящий Том/RealProjects/RealEstateWebSite/data_science/Data/train.csv")
+X_train, X_test, y_train, y_test = preprocess("/media/venchislav/Говорящий Том/RealProjects/RealEstateWebSite/data_science/Data/train.csv")
+print(X_train.head())
